1403d792eb6a3e6cf47f8b028f852d98c7191609
mlk96j <kaka_rel2010@hotmail.com>
2025-02-06 20:49:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     let dbMap = {
         "quran": "data/quran.json",
         "hadith": "data/hadith.json",
-        "fiqh": "https://raw.githubusercontent.com/mlk96j/dhakker/refs/heads/data/fiqh.json"
+        "fiqh": "https://raw.githubusercontent.com/mlk96j/dhakker/refs/main/data/fiqh.json"
     };
     
     if (dbMap[page]) {
